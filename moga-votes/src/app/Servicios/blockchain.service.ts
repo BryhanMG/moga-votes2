f2e793cdd07015695dd6a43a9d5193f42ee9a38c
BryhanMG <eddyforte@hotmail.com>
2019-07-28 23:56:35 -0600

    Deploy v1.0.5

--- moga-votes/src/app/Servicios/blockchain.service.ts
+++ moga-votes/src/app/Servicios/blockchain.service.ts
@@ -8,7 +8,7 @@ import { EmitirVoto, CandidatoBC, Voto  } from "src/app/Modelo/modelosBC";
   providedIn: 'root'
 })
 export class BlockchainService {
-  readonly URL_API = 'https://ec2-3-84-251-40.compute-1.amazonaws.com:3000/api/';
+  readonly URL_API = 'http://35.224.231.132:3000/api/';
 
 
   constructor(private http: HttpClient) { }
