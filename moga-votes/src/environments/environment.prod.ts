96a8bd491fb3623986bfa354dbd011ee82882f1a
BryhanMG <eddyforte@hotmail.com>
2019-08-26 14:57:24 -0600

    Deploy v2.1.2

--- moga-votes/src/environments/environment.prod.ts
+++ moga-votes/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCguZ3y0ImNYPIs3mRG1Cy5BFcSxjXT5-4",
+    authDomain: "moga-votes.firebaseapp.com",
+    databaseURL: "https://moga-votes.firebaseio.com",
+    projectId: "moga-votes",
+    storageBucket: "moga-votes.appspot.com",
+    messagingSenderId: "969750291752",
+    appId: "1:969750291752:web:4a266c7c9d8d0b8f"
+  }
 };
