6030d12c82b6671e6beb2056f17ea075901bc7ea
BryhanMG <43525966+BryhanMG@users.noreply.github.com>
2019-09-28 21:51:11 -0600

    Update database.js

--- server/database.js
+++ server/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 //direccion de la base de datos
 //const URI = 'mongodb://localhost:27017/moga';
-const URI = 'mongodb+srv://bryan:Pato123456@clustermoga-llumz.mongodb.net/moga?retryWrites=true';
+const URI = '';
 mongoose.connect(URI)
     .then(db => console.log('BD esta conectada'))
     .catch(err => console.error(err)); 
